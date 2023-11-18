286f2039de7e1d07164bec158d40c3fad112e852
Caleb de los Angeles <calebdla@gmail.com>
2023-11-17 19:27:36 -0800

    Update remove_spaces.bat
    Added parameters to file

--- remove_spaces.bat
+++ remove_spaces.bat
@@ -1,16 +1,37 @@
 @echo off
 
 rem  Remove spaces in a txt file and write to a new file.
-rem  Make the the following changes and run this batch file in the directory of your file:
-rem  1) Change "input_file.txt" on line 13 to the file you wish to alter
-rem  2) Change the space in "line: =!" on line 15 to what ever character you want to remove
-rem  3) Change "output_file.txt" on line 15 to the name of a new file to save to
 
-rem  TODO change these to parameters
+rem  Parameters (order matters!):
+rem  [-i] [inputFile] [-o] [outputFile] [-d] [delim]
 
-   setlocal enabledelayedexpansion
+rem  [-i] [inputFile]	name of file to be changed (default "input_file.txt")
+rem  [-o] [outputFile]	name of file to save changes to (default "output_file.txt")
+rem  [-d] [delim]	character to be removed (" " by default)
 
-   for /F "tokens=*" %%A in (input_file.txt) do (
+rem  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+setlocal disableDelayedExpansion
+
+rem initialize variables
+set "inputFile=input_file.txt"
+set "outputFile=output_file.txt"
+set "delim= "
+
+rem assign input file
+if "%~1"=="-i" (set "inputFile=%~2" & shift & shift)
+
+rem assign output file
+if "%~1"=="-o" (set "outputFile=%~2" & shift & shift)
+
+rem assign character to be removed 
+if "%~1"=="-d" (set "delim=%~2" & shift & shift)
+
+rem  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+setlocal enabledelayedexpansion
+
+for /F "tokens=*" %%A in (%inputFile%) do (
   set line=%%A
-echo(!line: =!>>output_file.txt
+  echo(!line:%delim%=!>>%outputFile%
  )
